b5e2729f331274fd4c34eb6247c64a904602865c
RSpe <me@rspe.xyz>
2019-08-12 19:18:03 +1200

    Finished inaccurate

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-import tensorflow
+import tensorflow as tf
+from tensorflow.keras import layers
 import pickle
 import tarfile
 import numpy as np
@@ -56,17 +57,80 @@ def normalise(pixels, rgb):
     
     return pixels
 
-def tf(w, h, rgb):
+def tf_reset(pixels, labels):
     
-    tf.reset_default_grapgh
+    tf.compat.v1.reset_default_graph()
+
+    test_set = unpickle("cifar-10-batches-py/test_batch")
+    test_pixels, test_labels, test_names = fix_input(test_set)
+
+    x_train = pixels
+    y_train = labels
+    x_test = test_pixels
+    y_test = test_labels
+
+    return x_train, y_train, x_test, y_test
+
+def tfk_model(x_train, y_train, x_test, y_test, num_classes):
+
+
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+    x_train = x_train.transpose(0, 2, 3, 1)
+    x_test = x_test.transpose(0, 2, 3, 1)
+    
+    model = tf.keras.models.Sequential()
+
+    # Convolutional layer 1
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), padding="same", input_shape = x_train.shape[1:]))
+    model.add(tf.keras.layers.Activation("elu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size = (2, 2)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    # Convolutional layer 2
+    model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), padding="same", input_shape = x_train.shape[1:]))
+    model.add(tf.keras.layers.Activation("elu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size = (2, 2)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    # Convolutional layer 3
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=(5, 5), padding="same", input_shape = x_train.shape[1:]))
+    model.add(tf.keras.layers.Activation("elu"))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size = (2, 2)))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    model.add(tf.keras.layers.Flatten())
+
+    #Fully connected layer 1
+    model.add(tf.keras.layers.Dense(1024))
+    model.add(tf.keras.layers.Activation("elu"))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.BatchNormalization())
+
+    #Fully connected layer 2
+    model.add(tf.keras.layers.Dense(num_classes))
+    model.add(tf.keras.layers.Activation("softmax"))
+
+    model.compile(optimizer = "rmsprop", loss = "categorical_crossentropy", metrics = ["accuracy"])
+
+    model.fit(x_train, y_train, epochs = 10, batch_size = 128)
+    score = model.evaluate(x_test, y_test, batch_size = 128)
+
+    print("Tests loss: ", score[0])
+    print("Tests accuracy: ", score[1])
 
 if __name__ == "__main__":
-    extract("cifar-10-python.tar.gz")
+    #extract("cifar-10-python.tar.gz")
     data = unpickle("cifar-10-batches-py/data_batch_1")
-    pixels, label, names = fix_input(data)
+    pixels, labels, names = fix_input(data)
     #print(pixels[0][0])
     median_filter(pixels, 3, 3)
     histogram_eq(pixels, 32, 32, 3)
     normalise(pixels, 3)
-    tf(32, 32, 3)
+    x_train, y_train, x_test, y_test = tf_reset(pixels, labels)
+    tfk_model(x_train, y_train, x_test, y_test, len(names))
     #print(pixels[0][0])
